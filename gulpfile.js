451e057565f0de007f92eaeecb09bf8c17a57608
rsi2940 <rishi2940@gmail.com>
2017-10-11 20:27:26 -0400

    added a bunch of tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,30 @@
 const gulp = require("gulp");
+const pump = require("pump");
+const composer = require("gulp-uglify/composer");
+const uglifyjs = require("uglify-es");
+const browserSync = require("browser-sync").create();
+
+const uglify = composer(uglifyjs, console);
 
 gulp.task("hello", function() {
   console.log("Hello everyone!!");
+});
+
+gulp.task("compress", function(cb) {
+  pump([gulp.src("src/js/*.js"), uglify(), gulp.dest("dist/js")], cb);
+});
+
+gulp.task("watch", ["browserSync"], function() {
+  gulp.watch("src/*.html", browserSync.reload);
+  gulp.watch("src/js/*.js", browserSync.reload);
+  gulp.watch("src/css/*.css", browserSync.reload);
+});
+
+//BrowserSync
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "src"
+    }
+  });
 });
---
+++ src/js/index.js
@@ -0,0 +1,70 @@
+const initialCats = [
+  {
+    name: "Alex",
+    clickCount: 0,
+    imgSrc: "img/cat1.jpg",
+    nicknames: ["Alexi", "A-Bone", "Mr. A"]
+  },
+  {
+    name: "John",
+    clickCount: 0,
+    imgSrc: "img/cat2.jpg",
+    nicknames: ["Jonny"]
+  },
+  {
+    name: "Sophia",
+    clickCount: 0,
+    imgSrc: "img/cat3.jpg",
+    nicknames: ["Sophie"]
+  },
+  {
+    name: "Matt",
+    clickCount: 0,
+    imgSrc: "img/cat4.jpg",
+    nicknames: ["Mr. M", "Matty"]
+  },
+  {
+    name: "Scott",
+    clickCount: 0,
+    imgSrc: "img/cat5.jpg",
+    nicknames: ["Scoty", "S-Boss", "Mr. S"]
+  }
+];
+const ViewModel = function() {
+  const self = this;
+
+  this.catList = ko.observableArray([]);
+  initialCats.forEach(catData => self.catList.push(new Cat(catData)));
+  this.currentCat = ko.observable(this.catList()[0]);
+
+  this.incrementCounter = function() {
+    return self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+  };
+
+  this.setCat = function(cat) {
+    self.currentCat(cat);
+  };
+};
+
+const Cat = function(data) {
+  this.clickCount = ko.observable(data.clickCount);
+  this.name = ko.observable(data.name);
+  this.imgSrc = ko.observable(data.imgSrc);
+  this.nicknames = ko.observableArray(data.nicknames);
+
+  this.level = ko.computed(function() {
+    let level = "New Born";
+    if (this.clickCount() >= 200) {
+      level = "Adult";
+    } else if (this.clickCount() >= 100) {
+      level = "Teen";
+    } else if (this.clickCount() >= 50) {
+      level = "Pre-Teen";
+    } else if (this.clickCount() >= 10) {
+      level = "Infant";
+    }
+    return level;
+  }, this);
+};
+
+ko.applyBindings(new ViewModel());
